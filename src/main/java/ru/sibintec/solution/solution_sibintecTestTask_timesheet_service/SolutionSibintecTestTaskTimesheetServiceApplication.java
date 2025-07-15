8b2e16622cd46febbe78aea14b021d30bc0a7ef3
NikitaShevnin <mrborman95@gmail.com>
2025-07-15 14:27:01 +0300

    start

---
+++ src/main/java/ru/sibintec/solution/solution_sibintecTestTask_timesheet_service/SolutionSibintecTestTaskTimesheetServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.sibintec.solution.solution_sibintecTestTask_timesheet_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionSibintecTestTaskTimesheetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionSibintecTestTaskTimesheetServiceApplication.class, args);
+	}
+
+}
