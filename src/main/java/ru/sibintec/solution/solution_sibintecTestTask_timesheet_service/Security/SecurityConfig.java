d8ffdd3bd57f5311a61575ecf9b7ab2f948954f4
NikitaShevnin <mrborman95@gmail.com>
2025-07-19 11:12:44 +0300

    добавлен SecurityConfig для настройки доступа к ендпоинтам

---
+++ src/main/java/ru/sibintec/solution/solution_sibintecTestTask_timesheet_service/Security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package ru.sibintec.solution.solution_sibintecTestTask_timesheet_service.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+/**
+ * Настройка безопасности приложения.
+ */
+public class SecurityConfig {
+
+    /**
+     * Основные правила безопасности.
+     */
+    @Bean
+    public SecurityFilterChain security(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+        return http.build();
+    }
+
+    /**
+     * Бин кодировщика паролей.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
